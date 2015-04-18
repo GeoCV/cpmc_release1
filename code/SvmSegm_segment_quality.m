97ed1e7515dd76eb81c03667535a199cd3c91079
baijm <3110100625@zju.edu.cn>
2015-04-19 00:04:28 +0800

    full ver, RF code to be added

--- code/SvmSegm_segment_quality.m
+++ code/SvmSegm_segment_quality.m
@@ -10,22 +10,22 @@ function Q = SvmSegm_segment_quality(img_name, exp_dir, masks, segm_quality_type
         masks = cell2mat(masks);
     end
 
-    name = [exp_dir 'SegmentationObject/' img_name '.bmp'];
-    %name = [exp_dir 'SegmentationObject/' img_name '.png'];
+    %name = [exp_dir 'SegmentationObject/' img_name '.bmp'];
+    name = [exp_dir 'SegmentationObject/' img_name '.png'];
     
     %return if mask does not exist
-    if(~exist(name,'file'))
-        Q = [];
-        return;
-    end
+    %if(~exist(name,'file'))
+    %    Q = [];
+    %    return;
+    %end
     
     %convert RGB BMP to grayscale
-    rgb_gt_seg = imread(name);
-    ground_truth_obj_segs = rgb2gray(rgb_gt_seg);
-    %ground_truth_obj_segs = imread(name);
+    %rgb_gt_seg = imread(name);
+    %ground_truth_obj_segs = rgb2gray(rgb_gt_seg);
+    ground_truth_obj_segs = imread(name);
 
     %label all antenna as 21
-    ground_truth_obj_segs(find(ground_truth_obj_segs)) = 21;
+    %ground_truth_obj_segs(find(ground_truth_obj_segs)) = 21;
     un = unique(ground_truth_obj_segs);
     un(un==0) = [];
     un(un==255) = [];
